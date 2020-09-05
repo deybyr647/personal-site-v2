7ffddebcd7ac0d4e563d88b8ce5a33169f76e3b2
deybyr647 <deybyr647@gmail.com>
2020-09-05 18:09:50 -0400

    Reverted link renaming in portfolio page

--- src/pages/portfolio/portfolioPage.js
+++ src/pages/portfolio/portfolioPage.js
@@ -9,28 +9,28 @@ import { RecycleNearMe, ReactToDoList, simplWeather, P5_Snake} from '../../image
 
 let projectList = [
     {
-        demoUrl: 'https://deybyr647.github.io/RecycleNearMe',
+        demoUrl: 'https://deybyr647.com/RecycleNearMe',
         name: 'RecycleNearMe',
         description: 'RecycleNearMe aims to educate its users about recycling, ecology, and related topics. The site also eases the process of recycling for its users by providing information on how to package recycle-able materials, and by using user geolocation to show nearby recycling centers within a certain radius of the users location. Map shows recycling center information, such as address and if the location is open/closed. Developed with HTML, CSS, JavaScript, Google Maps JavaScript API, Google Places API, GNews API, and NewsAPI',
         img: RecycleNearMe,
         githubUrl: 'https://github.com/deybyr647/RecycleNearMe'
     },
     {
-        demoUrl: 'https://deybyr647.github.io/simplWeather',
+        demoUrl: 'https://deybyr647.com/simplWeather',
         name: 'simplWeather',
         description: 'A simple site which shows current weather information for a given US zip code. Weather information provided by OpenWeatherMap API. Created with HTML, CSS, and JavaScript',
         img: simplWeather,
         githubUrl: 'https://github.com/deybyr647/simplWeather'
     },
     {
-        demoUrl: 'https://deybyr647.github.io/p5-snake.js/',
+        demoUrl: 'https://deybyr647.com/p5-snake.js/',
         name: 'P5-Snake.js',
         description: 'Snake.js, but created using the P5.js library, for added features and difficulty',
         img: P5_Snake,
         githubUrl: 'https://github.com/deybyr647/p5-snake.js/'
     },
     {
-        demoUrl: 'https://deybyr647.github.io/CN-React-ToDoList',
+        demoUrl: 'https://deybyr647.com/CN-React-ToDoList',
         name: 'React To Do List',
         description: 'Guided project, in which I created a To Do List app, along with some peers at a Google Code Next bootcamp using StackBlitz IDE, React.js and Bootstrap CSS',
         img: ReactToDoList,
